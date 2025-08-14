0bbc848a85a0ca807609fba08e2c20b2fdb77e3d
Hiroki Hamaguchi <hari64boli64@gmail.com>
2025-08-14 21:12:28 +0900

    format with black

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     entry_points={
         "console_scripts": [
-            'pygridsynth = pygridsynth.cli:main',
+            "pygridsynth = pygridsynth.cli:main",
         ],
     }
 )
